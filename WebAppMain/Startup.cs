fce83fbe54e05a37c95caf2faa6e947b027a4d0f
Ilya <pl.2003@list.ru>
2023-06-09 01:33:16 +0400

    front

--- WebAppMain/Startup.cs
+++ WebAppMain/Startup.cs
@@ -42,7 +42,6 @@ namespace WebAppMain
             services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
